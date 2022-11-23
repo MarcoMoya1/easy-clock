08c6066a4675f88924d08700463f93068b9eaef9
915marco <915marco@gmail.com>
2022-11-22 22:44:14 -0600

    js for all hands to get time

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Howdy!");
+
+const secondsHand = document.getElementById('seconds-hand')
+
+const minutesHand = document.getElementById('minutes-hand');
+
+const hoursHand = document.getElementById('hours-hand');
+
+function getTime() {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+}
+
+getTime();
